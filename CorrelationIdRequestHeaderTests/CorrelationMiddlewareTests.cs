d9bf16e2d66b3912fd36269b651e927fe534471c
Marcelo Janazi <marcelo.janazi@neon.com.br>
2021-05-21 14:33:56 -0300

    Including middleware and tests

---
+++ CorrelationIdRequestHeaderTests/CorrelationMiddlewareTests.cs
@@ -0,0 +1,61 @@
+using CorrelationIdRequestHeader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CorrelationIdRequestHeaderTests
+{
+    public class CorrelationMiddlewareTests
+    {
+        private const string CORRELATION_TOKEN_HEADER = "x-correlation-id";
+        [Fact]
+        public async Task CheckIfExistsCorrelationId_WhenHeadersContainXCorrelationId()
+        {
+            //ARRANGE
+            var guid = Guid.NewGuid().ToString();
+            var httpContextMoq = new Mock<HttpContext>();
+            var headers = new Dictionary<string, StringValues>() {
+                { "x-correlation-id", guid }
+            };
+            httpContextMoq.Setup(x => x.Request.Headers)
+                .Returns(new HeaderDictionary(headers));
+
+            var httpContext = httpContextMoq.Object;
+
+            var requestDelegate = new RequestDelegate((innerContext) => Task.FromResult(0));
+
+            //ACT
+            var middleware = new RequestHeaderCorrelationIdMiddleware(requestDelegate);
+            await middleware.InvokeAsync(httpContext);
+
+            //ASSERT
+            Assert.False(StringValues.IsNullOrEmpty(httpContext.Request.Headers[CORRELATION_TOKEN_HEADER]));
+            Assert.Equal(guid, httpContext.Request.Headers[CORRELATION_TOKEN_HEADER].ToString());
+        }
+
+        [Fact]
+        public async Task CheckIfExistsCorrelationId_WhenHeadersDontContainXCorrelationId()
+        {
+            //ARRANGE
+            var httpContextMoq = new Mock<HttpContext>();
+            var headers = new Dictionary<string, StringValues>();
+            httpContextMoq.Setup(x => x.Request.Headers)
+                .Returns(new HeaderDictionary());
+
+            var httpContext = httpContextMoq.Object;
+
+            var requestDelegate = new RequestDelegate((innerContext) => Task.FromResult(0));
+
+            //ACT
+            var middleware = new RequestHeaderCorrelationIdMiddleware(requestDelegate);
+            await middleware.InvokeAsync(httpContext);
+
+            //ASSERT
+            Assert.False(StringValues.IsNullOrEmpty(httpContext.Request.Headers[CORRELATION_TOKEN_HEADER]));
+        }
+    }
+}
---
+++ RequestHeaderCorrelationIdMiddleware/RequestHeaderCorrelationIdMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Threading.Tasks;
+
+namespace CorrelationIdRequestHeader
+{
+    public class RequestHeaderCorrelationIdMiddleware
+    {
+        public RequestHeaderCorrelationIdMiddleware(RequestDelegate next)
+        {
+            Next = next;
+        }
+        private const string CORRELATION_TOKEN_HEADER = "x-correlation-id";
+        public RequestDelegate Next { get; }
+        public ILogger<RequestHeaderCorrelationIdMiddleware> Logger { get; }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!(!StringValues.IsNullOrEmpty(context.Request.Headers[CORRELATION_TOKEN_HEADER])
+                && Guid.TryParse(context.Request.Headers[CORRELATION_TOKEN_HEADER], out Guid correlationId)))
+            {
+                correlationId = Guid.NewGuid();
+                context.Request.Headers.Add(CORRELATION_TOKEN_HEADER, correlationId.ToString());
+            }
+
+            await Next(context);
+        }
+    }
+
+    public static class RequestHeadersCorrealationIdMiddlewareExtensions
+    {
+        public static IApplicationBuilder AddRequestHeaderCorrelationId(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<RequestHeaderCorrelationIdMiddleware>();
+        }
+    }
+}
