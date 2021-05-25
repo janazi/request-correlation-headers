5f7cba0cdc449e25acda0c02a3851e507fef785b
Marcelo Janazi <marcelo.janazi@neon.com.br>
2021-05-25 14:28:58 -0300

    Option to include correlation id into httpclient requests

---
+++ CorrelationIdRequestHeaderTests/HttpClientRequestHeaderTests.cs
@@ -0,0 +1,70 @@
+﻿using CorrelationIdRequestHeader;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.Protected;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RequestHeaderCorrelationIdTests
+{
+    public class HttpClientRequestHeaderTests
+    {
+        private const string CORRELATION_TOKEN_HEADER = "x-correlation-id";
+        [Fact]
+        public async void ShouldCreate_CorrelationId()
+        {
+            var request = new HttpRequestMessage();
+            var innerHandlerMock = new Mock<DelegatingHandler>();
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var context = new DefaultHttpContext();
+            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
+
+            innerHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", request, ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { });
+
+            var handler = new HttpClientRequestHeadersHandler(mockHttpContextAccessor.Object)
+            {
+                InnerHandler = innerHandlerMock.Object
+            };
+
+            var invoker = new HttpMessageInvoker(handler);
+            var result = await invoker.SendAsync(request, default);
+
+            Assert.NotNull(request.Headers.GetValues(CORRELATION_TOKEN_HEADER));
+            var correlationId = Guid.Parse(request.Headers.GetValues(CORRELATION_TOKEN_HEADER).First());
+            Assert.IsType<Guid>(correlationId);
+        }
+
+        [Fact]
+        public async void ShouldNotChangeAnExisting_CorrelationId()
+        {
+            var correlationId = Guid.NewGuid().ToString();
+            var request = new HttpRequestMessage();
+
+            var innerHandlerMock = new Mock<DelegatingHandler>();
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var context = new DefaultHttpContext();
+            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
+            context.Request.Headers.Add(CORRELATION_TOKEN_HEADER, correlationId);
+
+            innerHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", request, ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { });
+
+            var handler = new HttpClientRequestHeadersHandler(mockHttpContextAccessor.Object)
+            {
+                InnerHandler = innerHandlerMock.Object
+            };
+
+            var invoker = new HttpMessageInvoker(handler);
+            var result = await invoker.SendAsync(request, default);
+            var correlationIdReturned = context.Request.Headers[CORRELATION_TOKEN_HEADER];
+            Assert.Equal(correlationId, correlationIdReturned);
+        }
+    }
+}
---
+++ RequestHeaderCorrelationIdMiddleware/HttpClientRequestHeadersHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CorrelationIdRequestHeader
+{
+    public class HttpClientRequestHeadersHandler : DelegatingHandler
+    {
+        public HttpClientRequestHeadersHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        private const string CORRELATION_TOKEN_HEADER = "x-correlation-id";
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!(!StringValues.IsNullOrEmpty(httpContextAccessor.HttpContext.Request.Headers[CORRELATION_TOKEN_HEADER])
+                && Guid.TryParse(httpContextAccessor.HttpContext.Request.Headers[CORRELATION_TOKEN_HEADER], out Guid correlationId)))
+            {
+                correlationId = Guid.NewGuid();
+                request.Headers.TryAddWithoutValidation(CORRELATION_TOKEN_HEADER, correlationId.ToString());
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+
+    public static class AddHttpClientRequestHeadersExtensions
+    {
+        /// <summary>
+        /// Propagate the x-correlation-id headers to HttpClient requests
+        /// </summary>
+        /// <param name="services"></param>
+        public static void PropagateCorrelationIdHeader(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.ConfigureAll<HttpClientFactoryOptions>(options =>
+            {
+                options.HttpMessageHandlerBuilderActions.Add(builder =>
+                {
+                    builder.AdditionalHandlers.Add(builder.Services.GetRequiredService<HttpClientRequestHeadersHandler>());
+                });
+            });
+        }
+    }
+}
